9176b07a8008c1de39e061d709cfb80bdf00cba5
eslam-salem <eslam.salem595@gmail.com>
2019-05-03 06:51:05 +0200

    inital commit

---
+++ memeApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  memeApp
+//
+//  Created by Eslam  on 2/21/19.
+//  Copyright © 2019 Eslam. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
---
+++ memeApp/MemeStruct.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  memeApp
+//
+//  Created by Eslam  on 3/19/19.
+//  Copyright © 2019 Eslam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText : String?
+    var bottomText : String?
+    var originalImage: UIImage?
+    var memedImage: UIImage?
+    
+}
